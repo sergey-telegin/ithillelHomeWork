fe610e44bff7f7cd0aaa1ed889709610457d7fa0
sergey-telegin <tssee.ram@gmail.com>
2023-03-29 23:00:15 +0200

    add HomeTask10

---
+++ HomeTask10/Functions.php
@@ -0,0 +1,76 @@
+<?php
+function fileExtension($fileSource)
+{
+    $fileExtension = mime2ext($fileSource['type']);
+    if (!$fileExtension) {
+
+       exit("<H1>Неверный тип файла</H1>");
+
+    }
+    return $fileExtension;
+}
+
+function moveDatafromPostTo(): array
+{
+    $worker = [];
+    foreach ($_POST as $dataItem => $dataValue) {
+        $worker[$dataItem] = $dataValue;
+    }
+    return $worker;
+}
+
+function checkUploadPhoto ($filesource){
+    $file = $filesource;
+    if (!empty($file['error']) || empty($file['tmp_name'])) {
+        switch ($file['error']) {
+            case 1:
+            case 2: $error = 'Превышен размер загружаемого файла.'; break;
+            case 3: $error = 'Файл был получен только частично.'; break;
+            case 4: $error = 'Файл не был загружен.'; break;
+            case 6: $error = 'Файл не загружен - отсутствует временная директория.'; break;
+            case 7: $error = 'Не удалось записать файл на диск.'; break;
+            case 8: $error = 'PHP-расширение остановило загрузку файла.'; break;
+            case 9: $error = 'Файл не был загружен - директория не существует.'; break;
+            case 10: $error = 'Превышен максимально допустимый размер файла.'; break;
+            case 11: $error = 'Данный тип файла запрещен.'; break;
+            case 12: $error = 'Ошибка при копировании файла.'; break;
+            default: $error = 'Файл не был загружен - неизвестная ошибка.'; break;
+        }
+        echo ($error);
+
+    }
+
+}
+
+function renameMoveFile($from, $to, $ceckDirectory){
+    if (file_exists($ceckDirectory) || mkdir($ceckDirectory, 0777, true)) {
+        rename($from, $to);
+    }
+}
+
+function workWithFiles($filesource, $num){
+    checkUploadPhoto($filesource);
+    $fileExtension = fileExtension($filesource);
+    $fileNewName = $_POST['name'] . $_POST['surname'];
+    $photoDirectory = "." . DIRECTORY_SEPARATOR . "Workers" . DIRECTORY_SEPARATOR . "$fileNewName" . DIRECTORY_SEPARATOR;
+    $fileLoadedPath = $filesource['tmp_name'];
+    $pathOfPhotoNew = $photoDirectory . $fileNewName.$num . "." . $fileExtension;
+    renameMoveFile($fileLoadedPath, $pathOfPhotoNew, $photoDirectory);
+    return $pathOfPhotoNew;
+}
+
+
+function arrayToOtherArray($filesArray){
+
+    $newArray= [];
+
+    foreach ($filesArray as $key => $propertyArray){
+
+         foreach ($propertyArray as $propertyKey => $propertyName){
+             $newArray[$propertyKey][$key]=$propertyName;
+         }
+
+
+    }
+    return $newArray;
+}
---
+++ HomeTask10/Server.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once('Mime.php');
+require_once('Functions.php');
+
+$worker = moveDatafromPostTo();
+
+$fileSource = $_FILES['photo'];
+$num = 0;
+$pathOfPhotoNew1 = workWithFiles($fileSource, $num);
+
+
+$fileSourceMulti = $_FILES['multiphoto'];
+
+$fileSourceMulti = arrayToOtherArray($fileSourceMulti);
+
+foreach ($fileSourceMulti as $fileSource) {
+    $num += 1;
+    $pathOfPhotoNew = workWithFiles($fileSource, $num);
+}
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+    <h1>Information is updated.</h1>
+    <p><span class="red-bold">Warning!</span> If you see any mistakes, call HR department.</p>
+</header>
+
+<div>
+    <?php
+    echo "<img class=\"img\" src= $pathOfPhotoNew1 >";
+    ?>
+</div>
+<div class="data-position">
+    <h2>Your data</h2>
+    <table>
+        <?php
+        foreach ($worker as $data => $itemOfdata) {
+            $printData = ucfirst($data);
+            echo "<tr><td>$printData:</td>" . "<td>$itemOfdata</td></tr>";
+        }
+        ?>
+    </table>
+    <h3>Your add files</h3>
+    <table>
+        <?php
+
+        foreach ($fileSourceMulti as $item) {
+            $printData = ucfirst($item['name']);
+            echo "<tr><td>$printData:</td>" . "</tr>";
+        }
+        ?>
+    </table>
+</div>
+
+</body>
+</html>
